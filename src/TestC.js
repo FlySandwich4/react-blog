d14bef63bb858f7ee4b94b7390e91c5b390a9891
Liam <liamwu4@gmail.com>
2023-09-19 03:30:18 -0700

    conflict

--- src/TestC.js
+++ src/TestC.js
@@ -5,6 +5,7 @@ const Test = () => {
     const str = "cout<<abc \n  \n \n \n \n\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\naaa\naaa\naaa\naaa \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \naaa \naaa\n\n\naaaaaa";
     return (<div>
         Testing Case:
+        <div>Hello</div>
         <TCodeBox title={"computer.js"} content={str}/>
         <TCodeBox title={"aaa"} content={str}/>
         <TCodeBox title={useFetch()} content={str+str}/>
