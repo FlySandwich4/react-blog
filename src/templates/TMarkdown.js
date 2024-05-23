cc18730fd9ba5c72412bf14d80617f4d15c26a1a
FlySandwich4 <liamwu4@gmail.com>
2024-05-23 00:31:35 -0700

    highlighter

--- src/templates/TMarkdown.js
+++ src/templates/TMarkdown.js
@@ -1,10 +1,37 @@
 import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
+import rehypeRaw from 'rehype-raw';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const TMarkdown = (props) => {
+    // return (
+    //     <Markdown rehypePlugins={[rehypeHighlight]}>{ props.content }</Markdown> 
+    // );
     return (
-        <Markdown remarkPlugins={[remarkGfm]}>{ props.content }</Markdown> 
-    );
+        <Markdown
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeRaw]}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+    
+              return !inline && match ? (
+                <SyntaxHighlighter style={dracula} PreTag="div" language={match[1]} {...props}>
+                  {String(children).replace(/\n$/, '')}
+                </SyntaxHighlighter>
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        >
+          {props.content}
+        </Markdown>
+      );
 }
  
 export default TMarkdown;
