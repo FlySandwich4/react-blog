66ae68623f23140cdcf9a7bdb933d2d4587dc22e
FlySandwich4 <liamwu4@gmail.com>
2025-02-25 23:13:33 -0800

    fix bug for link

--- src/Peer.js
+++ src/Peer.js
@@ -8,16 +8,16 @@ const Peer = () => {
 	return (
 		<div>
 			<TTitle title="Also check out my peers' website!" />
-			<a href={jerryWeb}>
+			<a href={jerryWeb} className="peer">
 				<h1>Jialuo (Jerry) Hu</h1>
 			</a>
 			<p>Pursuing MS CS @ UC San Diego | CS @ UC Irvine '24</p>
-			<a href={davidWeb}>
+			<a href={davidWeb} className="peer">
 				{" "}
 				<h1>Seongjin (David) Yoon</h1>
 			</a>
 			<p>MSCS @ UC Irvine | CS @ UC Irvine '24 | ICS Honors Program</p>
-			<a href={chuckWeb}>
+			<a href={chuckWeb} className="peer">
 				<h1>Junhao (Chuck) Wang</h1>
 			</a>
 			<p>Software Engineer at AWS Game Tech</p>
